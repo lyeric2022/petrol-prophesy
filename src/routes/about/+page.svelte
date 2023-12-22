6a32db9e319853e238ef59c06058da49a6f37379
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-12-22 02:22:11 -0800

    chore(about): update text descriptions

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,23 +4,19 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>About Petrol Prophesy</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		This is an application to visualize future petrol prices. 
+		This will be achieved by leveraging machine learning neutral networks, trained from historical data and events. 
+		In essence: 
 	</p>
 
-	<pre>npm create svelte@latest</pre>
+	<pre>current price * historic patterns * geopolitical trends ≈ future price </pre>
 
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
+		Interact with the app in the home page.
+		Explore the technologies and methodologies used in the research page.
+		For collaboration/recruitment, visit the contact page for more infomation. 
 	</p>
 </div>
