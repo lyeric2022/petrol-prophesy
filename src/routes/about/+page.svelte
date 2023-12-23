fcba99c8ad95049927b55aa1d60de693f8104979
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-12-22 22:20:14 -0800

    fix(about): change wording

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,15 +8,15 @@
 
 	<p>
 		This is an application to visualize future petrol prices. 
-		This will be achieved by leveraging machine learning neutral networks, trained from historical data and events. 
+		This will be achieved by leveraging machine learning neural networks, trained from historical data and events. 
 		In essence: 
 	</p>
 
 	<pre>current price * historic patterns * geopolitical trends ≈ future price </pre>
 
 	<p>
-		Interact with the app in the home page.
-		Explore the technologies and methodologies used in the research page.
-		For collaboration/recruitment, visit the contact page for more infomation. 
+		Visualize future gas prices in the home page.
+		Explore the technologies and methodologies used on the research page.
+		For collaboration/recruitment, visit the contact page for more information. 
 	</p>
 </div>
